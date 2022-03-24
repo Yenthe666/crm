6cd52c0c1f8d0636956b6865d166684cbe2e1390
Ward M <ward.marissen@gmail.com>
2022-03-24 09:24:03 +0100

    [ADD] lead_stage_email_reminders: Send email reminders when lead is in stage for x days

---
+++ lead_stage_email_reminders/models/__init__.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+from . import crm_lead
+from . import crm_stage
+from . import crm_team
+from . import lead_mail_rule
---
+++ lead_stage_email_reminders/models/crm_lead.py
@@ -0,0 +1,30 @@
+# -- coding: utf-8 --
+from odoo import models, fields, api
+from datetime import timedelta
+from datetime import date
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    def send_email_reminder(self):
+        """
+        Search all leads that are in a stage that is configured for reminders.
+        Then send out the reminder if it is the correct day.
+        """
+        stages_with_reminder = self.env['crm.stage'].search([
+            ('send_automatic_reminder', '=', True),
+            ('lead_mail_rule_ids', '!=', [])
+        ])
+
+        leads = self.env['crm.lead'].search([('stage_id', 'in', stages_with_reminder.ids)])
+
+        today = date.today()
+
+        for lead in leads:
+            for lead_rule in lead.stage_id.lead_mail_rule_ids:
+                reminder_day = lead_rule.send_reminder_after
+                new_reminder_date = (lead.date_last_stage_update + timedelta(reminder_day)).date()
+                if new_reminder_date == today:
+                    lead_rule.email_template_id.use_default_to = True
+                    lead.message_post_with_template(lead_rule.email_template_id.id)
