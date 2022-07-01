93dc963317e985896f86b23b35c63a86dd06c086
Ward M <ward.marissen@gmail.com>
2022-07-01 17:32:07 +0200

    [FIX] lead_stage_email_reminders: Use partner language when sending reminder

--- lead_stage_email_reminders/models/crm_lead.py
+++ lead_stage_email_reminders/models/crm_lead.py
@@ -21,9 +21,12 @@ class CrmLead(models.Model):
         today = date.today()
 
         for lead in leads:
-            for lead_rule in lead.stage_id.lead_mail_rule_ids.filtered(lambda rule:rule.active):
+            for lead_rule in lead.stage_id.lead_mail_rule_ids.filtered(lambda rule: rule.active):
                 reminder_day = lead_rule.send_reminder_after
                 new_reminder_date = (lead.date_last_stage_update + timedelta(reminder_day)).date()
-                if new_reminder_date == today:
+                if new_reminder_date <= today:
                     lead_rule.email_template_id.use_default_to = True
-                    lead.message_post_with_template(lead_rule.email_template_id.id)
+                    lead.with_context(lang=lead.partner_id.lang).message_post_with_template(
+                        lead_rule.email_template_id.id,
+                        email_layout_xmlid="mail.mail_notification_light"
+                    )
